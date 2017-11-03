544e559c1e4cda0cccf3e5c8983ccfdb632ac03b
Jordan Pearson <jspearson@pugetsound.edu>
2017-11-03 11:13:21 -0700

    Fixed test bug

--- modules/calendar/tests/server/cal-event.server.routes.tests.js
+++ modules/calendar/tests/server/cal-event.server.routes.tests.js
@@ -24,7 +24,7 @@ describe('CalEvent CRUD tests', function () {
 
   before(function (done) {
     // Get application
-    app = express.init(mongoose);
+    app = express.init(mongoose.connection.db);
     agent = request.agent(app);
 
     done();
